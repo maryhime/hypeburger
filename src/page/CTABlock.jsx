4482d40e3789b69a1299839c6af25d75b68b7882
Mary Therese Caseria <caseria.therese@gmail.com>
2023-06-23 15:11:17 +0800

    #4 Added Call To Action  Section

--- src/page/CTABlock.jsx
+++ src/page/CTABlock.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import HeadingsComponent from '../components/generic/HeadingsComponent'
+import Button from '../components/generic/Button'
 
 const CTABlock = () => {
   return (
-    <div className="bg-[url('/images/cta-bg.svg')] h-fit w-full bg-blend-darken bg-repeat relative flex flex-col py-[103px] justify-center gap-48 overflow-hidden">
+    <div className="bg-[url('/images/cta-bg.svg')] h-[600px] bg-no-repeat bg-cover bg-center w-full flex flex-col py-100 justify-center gap-48 overflow-hidden items-center">
+      <HeadingsComponent subheading={'Come visit us at your local HYPEBURGER location'} highlighted={'Taste'} heading={'the Hype'} />
+
+      <Button text={'Find Location'} />
 
     </div>
   )
